8e2f53b03378019b5bff0a320d93d34db336849d
CycleUser <cycleuser@cycleuser.org>
2022-11-23 21:05:55 +0800

    Update __init__.py

--- geoweb/__init__.py
+++ geoweb/__init__.py
@@ -8,30 +8,6 @@ from functools import partial
 import sys,re,os,csv
 from io import StringIO,BytesIO
 
-# table = ui.table({
-#     'columnDefs': [
-#         {'headerName': 'Name', 'field': 'name'},
-#         {'headerName': 'Age', 'field': 'age'},
-#     ],
-#     'rowData': [
-#         {'name': 'Alice', 'age': 18},
-#         {'name': 'Bob', 'age': 21},
-#         {'name': 'Carol', 'age': 42},
-#     ],
-# }).classes('max-h-40')
-
-# with ui.plot(figsize=(2.5, 1.8)):
-#     x = np.linspace(0.0, 5.0)
-#     y = np.cos(2 * np.pi * x) * np.exp(-x)
-#     plt.plot(x, y, '-')
-#     plt.xlabel('time (s)')
-#     plt.ylabel('Damped oscillation')
-
-# def update():
-#     table.options.rowData[0].age += 1
-#     table.update()
-
-# ui.button('Update', on_click=update)
 
 def handle_upload(e: UploadEventArguments) -> None:
     name = e.names
@@ -51,13 +27,17 @@ def extract_data(name = 'file_name', file_content =bytes() ):
     # df = pd.read_csv(data)
     df = pd.DataFrame()
     bytes_data = file_content
-    print(name)
+    if type(name)=='str':
+        print(name)
     if 'csv' in name:
         df = pd.read_csv(BytesIO(bytes_data))
     elif 'xls' in name:
         df = pd.read_excel(BytesIO(bytes_data),engine='openpyxl')
     else:
         pass
+    
+    table = ui.table({})
+    table.view.load_pandas_frame(df)
 
     columns_list= df.columns.tolist()
     Label_list = df['Label'].to_list()
@@ -74,26 +54,25 @@ def extract_data(name = 'file_name', file_content =bytes() ):
     make_plot(x= Alkali, y= Silica)
 
 
-def make_plot(x=[],y=[],color='green', marker='o', linestyle='dashed',linewidth=2, markersize=12,alpha=0.5):
+def make_plot(x=[],y=[],color='green', marker='o', linestyle='dashed',linewidth=2, markersize=12,alpha=0.5,xlabel='x',ylabel='y'):
     diagram = ui.plot()
-    # diagram.figsize=(2.5, 1.8)
-    x = x
-    y = y
     plt.plot(x, y, color=color, marker=marker, linestyle=linestyle,linewidth=linewidth, markersize=markersize,alpha=alpha)
-    plt.xlabel('x')
-    plt.ylabel('y')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     diagram.view.set_figure(plt.gcf())
     img_tosave =plt.gcf()
+
     ui.button('Save IMG', on_click=saveIMG)
 
 
 def saveIMG():
-    plt.savefig("vector.svg")
+    plt.savefig('vector.svg')
+    ui.link('Vector File', 'vector.svg')
+
 
 def main():
 
     ui.upload(on_upload=handle_upload)
     ui.run()
-
 
 main()
